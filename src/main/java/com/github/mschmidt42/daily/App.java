4a50db04fc9eb5c757f3e88e24c0fb962205e303
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2023-02-28 22:08:52 +0100

    larger font

--- src/main/java/com/github/mschmidt42/daily/App.java
+++ src/main/java/com/github/mschmidt42/daily/App.java
@@ -1,5 +1,6 @@
 package com.github.mschmidt42.daily;
 
+import java.awt.Font;
 import javax.swing.*;
 
 public class App 
@@ -7,10 +8,11 @@ public class App
     public static void main( String[] args )
     {
         // Create a new JFrame window
-        JFrame frame = new JFrame("Always On Top Example");
+        JFrame frame = new JFrame("Daily");
 
         // Create a new JLabel with the three bullet points
         JLabel label = new JLabel("<html><ul><li>Was habe ich gestern erreicht?</li><li>Was habe ich heute geplant?</li><li>Wobei habe ich Probleme?</li></ul></html>");
+        label.setFont(new Font("Arial", Font.PLAIN, 20));
 
         // Add the label to the frame's content pane
         frame.getContentPane().add(label);
@@ -18,8 +20,9 @@ public class App
         // Set the frame to always be on top
         frame.setAlwaysOnTop(true);
 
+        
         // Set the size of the frame
-        frame.setSize(400, 150);
+        frame.setSize(500, 200);
 
         // Set the default close operation of the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
