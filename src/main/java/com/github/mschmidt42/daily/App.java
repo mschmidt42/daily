a8a220f311a6aef82fadd14b987662f3758b2d6a
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2023-02-28 21:58:41 +0100

    copy code from ChatGPT

--- src/main/java/com/github/mschmidt42/daily/App.java
+++ src/main/java/com/github/mschmidt42/daily/App.java
@@ -1,13 +1,30 @@
 package com.github.mschmidt42.daily;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Create a new JFrame window
+        JFrame frame = new JFrame("Always On Top Example");
+
+        // Create a new JLabel with the three bullet points
+        JLabel label = new JLabel("<html><ul><li>Was habe ich gestern erreicht?</li><li>Was habe ich heute geplant?</li><li>Wobei habe ich Probleme?</li></ul></html>");
+
+        // Add the label to the frame's content pane
+        frame.getContentPane().add(label);
+
+        // Set the frame to always be on top
+        frame.setAlwaysOnTop(true);
+
+        // Set the size of the frame
+        frame.setSize(400, 150);
+
+        // Set the default close operation of the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Display the frame
+        frame.setVisible(true);
     }
 }
