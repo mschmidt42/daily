94cb2d4e0a6b4de0f98e3aa3ea0c7ac8f944ba58
Michael Schmidt <michael.schmidt@opitz-consulting.com>
2023-03-01 18:03:22 +0100

    add icon

--- src/main/java/com/github/mschmidt42/daily/App.java
+++ src/main/java/com/github/mschmidt42/daily/App.java
@@ -1,21 +1,25 @@
 package com.github.mschmidt42.daily;
 
-import java.awt.Font;
 import javax.swing.*;
+import java.net.URL;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        new App().appMain(args);
+    }
+
+    public void appMain(String[] args) {
         // Create a new JFrame window
         JFrame frame = new JFrame("Daily");
 
         // Create a new JLabel with the three bullet points
-         JLabel label = new JLabel("<html>" + 
-            "<ul style='font-size: 20pt; line-height: 30pt;'>"  +
+        JLabel label = new JLabel("<html>" + 
+            "<style>li { margin-bottom: 0px; }</style>" +
+            "<ul style='font-size: 20pt;'>"  +
             "<li>Was habe ich gestern erreicht?</li>" +
             "<li>Was habe ich heute geplant?</li>" +
-            "<li>Wobei habe ich Probleme?</li>" +
+            "<li>Was hält mich auf?</li>" +
             "</ul></html>");
 
 
@@ -25,9 +29,15 @@ public class App
         // Set the frame to always be on top
         frame.setAlwaysOnTop(true);
 
+        // Set icon from resources
+        URL imgUrl = getClass().getClassLoader().getResource("icon.png");
+        ImageIcon icon = new ImageIcon(imgUrl);
+        frame.setIconImage(icon.getImage());
         
         // Set the size of the frame
-        frame.setSize(600, 260);
+        frame.setSize(500, 200);
+
+        frame.setLocation(700, 0);
 
         // Set the default close operation of the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
